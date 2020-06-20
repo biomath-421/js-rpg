d10acd738dadbedf7ad17f9abea8dd52a8cee8f7
AhmedSoleiman <ahmed.soleiman.mad@gmail.com>
2020-06-20 21:31:43 +0200

    js modifs

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -1,9 +1,9 @@
 document.getElementById("btnHitP1");
 document.getElementById("btnHealP1");
-document.getElementById("yieldP1");
+document.getElementById("btnYieldP1");
 document.getElementById("btnHitP2");
 document.getElementById("btnHealP2");
-document.getElementById("btn-yieldP2");
+document.getElementById("btnY ieldP2");
 
 let tourPlayer1 = true;
 let tourPlayer2 = false;
@@ -22,23 +22,22 @@ do {
   if (tourPlayer1) {
     console.log("player1 attaque");
     tourPlayer1 = false;
+    console.log(`score player 2 => ${(scorePlayer2 -= 10)}`);
     tourPlayer2 = true;
-
-    console.log(`score player 2 => ${(scorePlayer2 -= 1)}`);
+    if (scorePlayer2 < 0) {
+      tourPlayer2 = false;
+    }
   }
 
   if (tourPlayer2) {
     console.log("player2 attaque");
+    tourPlayer1 = false;
+    console.log(`score player 1 => ${(scorePlayer1 -= 10)}`);
     tourPlayer1 = true;
-    tourPlayer2 = false;
-    console.log(`score player 1 => ${(scorePlayer1 -= 2)}`);
+    if (scorePlayer1 < 0) {
+      tourPlayer1 = false;
+    }
   }
 } while (scorePlayer1 > 0 && scorePlayer1 > 0);
 
 console.log("-----------------------------------------");
-
-if (scorePlayer1 > scorePlayer2) {
-  console.log("Player 1 a gagne");
-} else {
-  console.log("Player 2 a gagne");
-}
